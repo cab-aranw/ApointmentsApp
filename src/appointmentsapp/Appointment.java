01659a0b3e025f0fed884e9a98782473b1402b91
Lyceum <48872358+cab-aranw@users.noreply.github.com>
2019-09-27 02:17:10 -0400

    Implementando la clase Appointments
    implementando appointments

--- src/appointmentsapp/Appointment.java
+++ src/appointmentsapp/Appointment.java
@@ -9,6 +9,67 @@ package appointmentsapp;
  *
  * @author Cristy
  */
-public class Appointment {
+
+public abstract class Appointment 
+{
+    private String lastName ;
+    private String description;
+    private int militaryHour;
+    private int militaryTime;
+    private int militaryMinutes;
+
+    public Appointment(String lastName, String description, int militaryHour, int militaryTime, int militaryMinutes) 
+    {
+        this.lastName = lastName;
+        this.description = description;
+        this.militaryHour = militaryHour;
+        this.militaryTime = militaryTime;
+        this.militaryMinutes = militaryMinutes;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getMilitaryHour() {
+        return militaryHour;
+    }
+
+    public void setMilitaryHour(int militaryHour) {
+        this.militaryHour = militaryHour;
+    }
+
+    public int getMilitaryTime() {
+        return militaryTime;
+    }
+
+    public void setMilitaryTime(int militaryTime) {
+        this.militaryTime = militaryTime;
+    }
+
+    public int getMilitaryMinutes() {
+        return militaryMinutes;
+    }
+
+    public void setMilitaryMinutes(int militaryMinutes) {
+        this.militaryMinutes = militaryMinutes;
+    }
+    
+    
+    
+    public abstract boolean occursOn(int year, int month, int day);
+    
     
 }
--- src/appointmentsapp/AppointmentsApp.java
+++ src/appointmentsapp/AppointmentsApp.java
@@ -5,50 +5,90 @@
  */
 package appointmentsapp;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Cristy
  */
 public class AppointmentsApp {
-    public Appointment[] myAppointments;
+    static public Appointment[] myAppointments;
     public int currentSize = 0;
+    static Scanner keyboard = new Scanner (System.in);
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    
+    public static void main(String[] args) 
+    {
          AppointmentsApp myApp = new AppointmentsApp();
          
+         
+         
         //call 2 methods:
-        //  myApp.makeAppointments();
-        //  myApp.checkAppointments();
+        myApp.makeAppointments();
+        myApp.checkAppointments();
+         //System.out.println("XXXXXXX");
 
     }
-    
     //In the makeAppointments method, do the following:
         /**
-        * 
-        * 
         * How many appointments do you wish to make? 3
-        * 
         * Use the number entered by user to instantiate the array of appointments to that size.
-        * 
         * Create a menu for-loop as follows:
             Please make a selection:
             1. One Time Appointment
             2. Daily Appointment 
             3. Monthly Appointment 
-            * 
             * For Each option, call a specific method that will request the appropriate data from the user, 
             * and then instantiate the appropriate class/subclass in the Appointment hierarchy
             * For each appointment made, you will add it to the global array of appointments at the currentSize,
             * and increment a currentSize global variable that keeps track of the next empty slot in the array.
-            * 
-            * 
-            * 
-            * 
-            * 
+            */
+    public static void makeAppointments ()
+    {
+        System.out.println("How many appointments do you wish to make  ? :");
+        int numAppointments = keyboard.nextInt();
+        myAppointments = new Appointment[numAppointments];
+        for (int i = 0; i < numAppointments; i++) 
+        {
+            boolean menu = false;
+            do 
+            {
+                System.out.println("Please make a selection :");
+                System.out.println("\t 1. One Time Appointment");
+                System.out.println("\t 2. Daily Appointment");
+                System.out.println("\t 3. Monthly appointment");
+                int selection = keyboard.nextInt();
+                switch (selection) 
+                {
+                    case 1:
+                        System.out.println("Calling the class one time App ..");
+                        menu = true;
+                        break;
+                    case 2:
+                        System.out.println("Calling the daily Appointment ..");
+                        menu = true;
+                        break;
+                    case 3:
+                        System.out.println("Calling monthly App ..");
+                        menu = true;
+                        break;
+                    default:
+                        throw new AssertionError();
+                        
+                }
+            } while (!menu);
+            
+          
+            
+            
+        }
+        
+    }
+    
     //In the checkAppointments method, do the following:
-        * Inside a do-while loop, ask the user the following questions:
+        /** Inside a do-while loop, ask the user the following questions:
             * What is the date you wish to look up in your Appointments' Calendar? 
                     Enter the month: 03
                     Enter the day: 15
@@ -69,7 +109,11 @@ public class AppointmentsApp {
 
 
         */
+    
+    public static void checkAppointments()
+    {
         
+    }
         
         
     }
